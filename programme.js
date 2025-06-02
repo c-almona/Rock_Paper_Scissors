e027ffc3912bf02df13a2cc76b09c8850eaf5b18
c-almona <chidinmaalmona@gmail.com>
2025-06-02 21:16:16 +0100

    Created the logic for a single round created variables to keep score created the logic to determine the winner of a single round and update the scores.

--- programme.js
+++ programme.js
@@ -29,3 +29,52 @@ function getPlayerChoice() {
 }
 //this calls the function to get player's choice
 getPlayerChoice();
+
+//Initialising score variables
+let playerScore = 0;
+let computerScore = 0;
+
+//this is the logic for a single round
+function playRound(getPlayerChoice, getComputerChoice) {
+    if (getPlayerChoice == "rock" && getComputerChoice == "paper")
+    {
+        console.log("You lose! Paper beats Rock");
+        computerScore ++;
+    }
+
+    else if (getPlayerChoice == "rock" && getComputerChoice == "scissors")
+    {
+        console.log("You win! Rock beats Scissors");
+        playerScore ++;
+    }
+
+    else if (getPlayerChoice == "paper" && getComputerChoice == "rock")
+    {
+        console.log("You win! Paper beats Rock");
+        playerScore ++;
+    }
+
+    else if (getPlayerChoice == "paper" && getComputerChoice == "scissors")
+    {
+        console.log("You lose! Scissors beats Paper");
+        computerScore ++;
+    }
+
+    else if (getPlayerChoice == "scissors" && getComputerChoice == "rock")
+    {
+        console.log("You lose! Rock beats Scissors");
+        computerScore ++;
+    }
+
+    else if (getPlayerChoice == "scissors" && getComputerChoice == "paper")
+    {
+        console.log("You win! Scissors beats Paper");
+        playerScore ++;
+    }
+
+    else
+    {
+        console.log("It's a tie!");
+
+    }
+}
